30f90b43e65e1f2b85bf273fc31013813e090ade
shakibaam <shakibaamirshahi@yahoo.com>
2020-07-29 17:48:03 +0330

    connect added

---
+++ connect_DB.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class connect_DB {
+
+    public static void main(String[] args) {
+
+//        Scanner sc=new Scanner(System.in);
+//        String one=sc.next();
+//        System.out.println(one);
+//        sc.nextLine();
+//        String tow=sc.nextLine();
+//        System.out.println(tow);
+//        String three=sc.next();
+//        System.out.println(three);
+
+        System.out.println("hello");
+        Scanner scanner=new Scanner(System.in);
+
+
+    }
+
+}
